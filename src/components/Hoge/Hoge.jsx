6578244553087255015f8fa78e9bfb0972c71098
junkisai <jonki.beam@gmail.com>
2019-10-01 15:02:21 +0900

    New: components/のテンプレ追加

---
+++ src/components/Hoge/Hoge.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+
+const Hoge = props => {
+  return (
+    <div></div>
+  );
+};
+
+Hoge.propTypes = {
+};
+
+Hoge.defaultTypes = {
+};
+
+export default Hoge;
